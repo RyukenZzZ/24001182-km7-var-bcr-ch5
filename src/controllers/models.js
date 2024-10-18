94523f7b124fa0c8a9aded28f85cbab336df21be
yantoꦪꦤ꧀ꦠꦺꦴ <147719517+chawnima@users.noreply.github.com>
2024-10-18 21:44:51 +0700

    feature: Create Type (#3)
    * feature: Create Type
    
    * Revert "fix conflict"
    
    This reverts commit 1d8163f5924925bfe4a1e295f3604c624bfba5bb, reversing
    changes made to c5fdf37721a36a1c5f82382e1e0e10f5652da274.
    
    * add remaining crud

--- src/controllers/models.js
+++
@@ -1,8 +0,0 @@
-const modelService = require("../services/models");
-const { successResponse } = require("../utils/response");
-
-exports.createModel = async (req, res, next) => {
-    // Create the new Car
-    const data = await modelService.createModel(req.body);
-    successResponse(res, data, "Model successfully added!");
-};
--- src/services/models.js
+++
@@ -1,7 +0,0 @@
-const modelRepository = require("../repositories/model");
-const { NotFoundError, InternalServerError } = require("../utils/request");
-
-exports.createCar = async (data, file) => {
-    // Create the data
-    return modelRepository.createCar(data);
-};
