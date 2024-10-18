c32945593dfa66057f1f2f9cb630f9842f3bfa0c
yantoꦪꦤ꧀ꦠꦺꦴ <147719517+chawnima@users.noreply.github.com>
2024-10-18 22:03:24 +0700

    Feature create type (#4)
    * feature: Create Type
    
    * Revert "fix conflict"
    
    This reverts commit 1d8163f5924925bfe4a1e295f3604c624bfba5bb, reversing
    changes made to c5fdf37721a36a1c5f82382e1e0e10f5652da274.
    
    * add remaining crud
    
    * add

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+
 const carsRouter = require("./cars");
-const modelsRouter = require("./models")
+const modelsRouter = require("./models");
+const typeRouter = require("./type");
 
 const router = express.Router();
 
 router.use("/cars", carsRouter);
 router.use("/models", modelsRouter);
+router.use("/type", typeRouter);
 
 module.exports = router;
